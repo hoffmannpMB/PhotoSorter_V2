0cc6d83df0224d4478f3a4863182a2bcaefd2d75
Phillip Hoffmann <phillip.hoffmann1989@outlook.com>
2019-05-12 23:49:21 +0200

    PhotoRepository added

---
+++ Repository/IPhotoRepository.cs
@@ -0,0 +1,12 @@
+﻿using Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Repository
+{
+    public interface IPhotoRepository
+    {
+        Task<IReadOnlyList<StorageFile>> GetPhotosAsync();
+    }
+}
---
+++ Repository/Implementations/PhotoRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
+namespace Repository.Implementations
+{
+    internal class PhotoRepository : IPhotoRepository
+    {
+        public async Task<IReadOnlyList<StorageFile>> GetPhotosAsync()
+        {
+            var picker = new FileOpenPicker
+            {
+                ViewMode = PickerViewMode.Thumbnail, SuggestedStartLocation = PickerLocationId.PicturesLibrary
+            };
+
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
+
+            return await picker.PickMultipleFilesAsync();
+        }
+    }
+}
